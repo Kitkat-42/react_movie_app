ed749278d2fd771f8a946a863886fde9225522b5
Seoyoung Jeong <issyo1022@gmail.com>
2021-08-22 13:25:08 +0900

    feat: dynamic component generation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Potato from "./Potato";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /><Potato />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
